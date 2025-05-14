77cead267a186f77b3b29fed70bafd6853e5a4c6
Dani Ihza Farrosi <dani.videda@gmail.com>
2025-05-14 17:12:59 +0700

    add problemset

---
+++ codeforces/2108A/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++)
+    {
+        int num;
+        long long ans;
+
+        cin >> num;
+        ans = ((num * num) / 4) + 1;
+
+        cout << ans << '\n';
+    }
+    
+
+    return 0;
+}
